8958e8f6a63c60584777f6c12357a92ec7280e2f
Parth Maheshwari <37042876+parthpm@users.noreply.github.com>
2019-01-21 20:51:47 +0530

    Add files via upload
    For Polynomial

---
+++ BisectionMethod2.cpp
@@ -0,0 +1,113 @@
+#include<iostream>
+#include<iomanip>
+#include<math.h>
+using namespace std;
+float compute(int a[],int deg,float value);
+float absu(float f);
+
+int main()
+{
+    float result;
+    int degree;
+
+    cout<<"Enter the degree of the eqn\n";
+    cin>>degree;
+
+    int a[degree];
+    int temp=degree;
+
+    while(temp>=0)
+    {   cout<<"Enter the coeff of x^ "<<temp<<endl;
+        cin>>a[temp];
+        temp--;
+    }
+
+    float left,right,m;
+    int accuracy;
+    cout<<"Enter accuracy\n";
+    cin>>accuracy;
+    float arr=pow(10,-accuracy);
+    cout<<"\n"<<arr;
+
+
+
+    cout<<"Enter left and right limit respectively:\n";
+    cin>>left>>right;
+    if(compute(a,degree,left)>compute(a,degree,right))
+    {
+        float temp;
+        temp=left;
+        left=right;
+        right=temp;
+    }
+
+    float temp1;
+
+    do
+    {
+        m=(left+right)/2;
+        result=compute(a,degree,m);
+      // cout<<" M is:"<<m<<"\n";
+
+    if(result>0)
+    {
+       right=m;
+    }
+    else
+        left=m;
+    temp1=compute(a,degree,(right+left)/2);
+    //cout<<"Temp is:" <<temp;
+
+    }while(absu(result-temp1)>arr);
+
+    cout<<"Answer is :"<<m;
+
+
+
+
+
+    return 0;
+}
+
+float compute(int a[],int deg,float value)
+{
+    int temp=deg;
+    float result=0;
+    while(temp>=0)
+    {
+        result+=a[temp]*pow(value,temp);
+        temp--;
+    }
+
+    return result;
+}
+
+float absu(float f)
+{
+    if(f>0)
+    {
+        return f;
+    }
+    else
+    {
+        return -f;
+    }
+}
+
+/*float compute(float left,float right,float a,float b,float c,float x)
+{
+    float m=(left+right)/2
+    float result=quad(a,b,c,x);
+
+    if(result>0)
+    {
+       right=m;
+    }
+    else
+        left=m;
+
+    return m;
+
+
+
+}*/
